bec6f5e77527d2608b684530a744d5a8d5e5531f
shubham <shubhamlonkar2294@gmail.com>
2020-06-20 15:23:16 +0530

    added pug files and modified code accordingly

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,23 +1,25 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','pug')
+app.set('views','views')
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).send('<h1>Page not found</h1>');
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404')
 });
 
 app.listen(3000);
--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -1,13 +1,18 @@
 const path = require('path');
-const express = require('express');
 
-const router = express.Router();
+const express = require('express');
 
 const rootDir = require('../util/path');
 
+const router = express.Router();
+
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
-  //res.send('<h1>Hello from Express!</h1>');
-  res.sendFile(path.join(rootDir,'views','shop.html'));
+  // console.log('Shop js',adminData.products)
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
