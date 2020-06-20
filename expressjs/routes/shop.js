4bf56190c99d5e80ccb5e9300a9f621ba6b2e098
shubham <shubhamlonkar2294@gmail.com>
2020-06-20 18:53:31 +0530

    added main layout file of handlebars and imported in other html handlerars files and modified code of handlebars(.hbs) files and js files accordingly

--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -12,7 +12,14 @@ router.get('/', (req, res, next) => {
   // console.log('Shop js',adminData.products)
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
